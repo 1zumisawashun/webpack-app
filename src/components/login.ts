290f4dcd86db943820df566c24be3c3c16fa8d83
1zumisawashun <shun.1zumisawa@gmail.com>
2022-05-05 00:33:31 +0900

    login機能をクラス化

---
+++ src/components/login.ts
@@ -0,0 +1,34 @@
+import loginIcon from "../assets/login.svg";
+
+export class Login {
+  form: HTMLFormElement;
+  loginImage: HTMLElement;
+
+  constructor() {
+    this.form = document.querySelector("form")!;
+    this.loginImage = document.getElementById("loginImg")!;
+    this.configure();
+    this.renderImage();
+  }
+
+  configure() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const data = this.formData(this.form);
+      console.log(data, "data");
+    });
+  }
+
+  renderImage() {
+    (this.loginImage as HTMLInputElement).src = loginIcon;
+  }
+
+  formData = (form: HTMLFormElement) => {
+    const inputs = form.querySelectorAll("input");
+    let values: { [prop: string]: string } = {};
+    inputs.forEach((input) => {
+      values[input.id] = input.value;
+    });
+    return values;
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import "./styles/tab.scss";
 import "./styles/project.scss";
 import "./styles/login.scss";
 
-import "./login";
+import { Login } from "./components/login";
 import { ProjectList } from "./components/project-list";
 import { ProjectInput } from "./components/project-input";
 
+new Login();
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
